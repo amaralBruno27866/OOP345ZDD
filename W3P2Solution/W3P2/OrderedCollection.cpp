5cdf3cf01ebd8c3b20df61c7e891e720b3084cb6
bruno27866 <bamaral2.git2023@gmail.com>
2024-05-29 09:35:39 -0400

    Uploding folder with W3P2Solution

---
+++ W3P2Solution/W3P2/OrderedCollection.cpp
@@ -0,0 +1,9 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include "OrderedCollection.h"
+
+using namespace std;
+namespace seneca {
+
+}
---
+++ W3P2Solution/W3P2/OrderedCollection.h
@@ -0,0 +1,12 @@
+#ifndef SENECA_ORDERED_COLLECTION_H
+#define SENECA_ORDERED_COLLECTION_H
+
+#include <iostream>
+
+namespace seneca {
+	class OrderedCollection
+	{
+	};
+}
+
+#endif // !SENECA_ORDERED_COLLECTION_H
