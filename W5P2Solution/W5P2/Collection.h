3650d29678187c005584597d35d321d18b7d20bb
bruno27866 <bamaral2.git2023@gmail.com>
2024-06-14 15:32:48 -0400

    change: Collection.h

--- W5P2Solution/W5P2/Collection.h
+++ W5P2Solution/W5P2/Collection.h
@@ -1,42 +1,72 @@
 #ifndef SENECA_COLLECTION_H
 #define SENECA_COLLECTION_H
 
-#include <cstddef>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
 #include <stdexcept>
 
 namespace seneca {
 	template <typename T>
 	class Collection {
 	private:
-		T* items;
-		size_t size;
+		std::string m_name;
+		std::vector<T> m_items;
+		void (*m_observer)(const Collection<T>&, const T&);
 	public:
-		Collection() : items(nullptr), size(0) {}
-		~Collection() { delete[] items; }
+		Collection(const std::string& name):m_name(name), m_observer(nullptr) {}
+		const std::string& name() const { return m_name; };
+		size_t size() const { return items.size(); };
 
-		void add(const T& item) {
-			T* temp = new T[size + 1];
-			for (size_t i = 0; i < size; i++) {
-				temp[i] = items[i];
-			}
-			temp[size] = item;
-			delete[] items;
-			items = temp;
-			size++;
-
-			if (callback) {
-				callback(item);
-			}
+		void setObserver(void(*obeserver)(const Collection<T>&, const T&)) {
+			m_observer = obeserver;
 		}
 
-		T& operator[](size_t index) {
-			if (index >= size) {
-				throw std::out_of_range("Invalid index");
+		Collection<T>& operator+=(const T& item) {
+			auto isTitleMatch = & {return i.title() == item.title(); };
+
+			auto it = std::find_if(m_items.begin(), m_items.end(). isTitleMatch);
+
+			bool itemExists = it != m_items.end();
+
+			if (!itemExists) {
+				m_items.push_back(item);
+				if (m_observer) {
+					m_observer(*this, item);
+				}
 			}
-			return items[index];
+
+			return *this;
 		}
 
-		size_t getSize() const { return size; }
+		T& operator[](size_t idx) const {
+			if (idx >= m_items.size()) {
+				std::string errorMessage = "Bad index [";
+				errorMessage += std::to_string(idx);
+				errorMessage += "]. Collection has [";
+				errorMessage += std::to_string(m_items.size());
+				errorMessage += "] items.";
+				throw std::out_of_range(errorMessage);
+			}
+
+			return m_items[idx];
+		}
+
+		T* operator[](const std::string& title) const {
+			auto isTitleMatch = [&](const T& i) { return i.title() == title; };
+
+			auto it = std::find_if(m_items.begin(), m_items.end(), isTitleMatch);
+
+			return it != m_items.end() ? &(*it) : nullptr;
+		}
+
+		friend std::ostream& operator<<(std::ostream& os, const Collection<T>& collection) {
+			for (const auto& item : collection.m_items) {
+				os << item;
+			}
+			return os;
+		}
 	};
 }
 #endif // !SENECA_COLLECTION_H
