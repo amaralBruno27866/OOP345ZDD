5849c39d2a56a616a29a20c959087b818538e117
bruno27866 <bamaral2.git2023@gmail.com>
2024-06-15 14:49:16 -0400

    fix: SpellChecker.h - 16 | fix: SpellChecker.cpp - SpellChecker(arg)

--- W5P2Solution/W5P2/SpellChecker.cpp
+++ W5P2Solution/W5P2/SpellChecker.cpp
@@ -1,36 +1,40 @@
 #define _CRT_SECURE_NO_WARNINGS
-#include <iostream>
+#include "SpellChecker.h"
 #include <fstream>
 #include <algorithm>
 #include <iomanip>
-#include "SpellChecker.h"
+#include <stdexcept> // For std::runtime_error
 
 namespace seneca {
-	SpellChecker::SpellChecker(const char* filename) {
-		std::ifstream file(filename);
-		if (!file) throw "Bad file name!";
-		std::string line;
-		for (size_t i = 0; i < 6 && getline(file, line); ++i) {
-			auto pos = line.find(' ');
-			m_badWords[i] = line.substr(0, pos);
-			m_goodWords[i] = line.substr(line.find_last_not_of(' ') + 1);
-		}
-	}
+    SpellChecker::SpellChecker(const char* filename) {
+        std::ifstream file(filename);
+        if (!file) throw std::runtime_error("Bad file name!");
+        std::string line;
+        for (size_t i = 0; i < 6 && std::getline(file, line); ++i) {
+            auto pos = line.find(' ');
+            m_badWords[i] = line.substr(0, pos);
+            // Find the start of the good word
+            auto start = line.find_first_not_of(' ', pos);
+            // Find the end of the good word
+            auto end = line.find_last_not_of(' ');
+            m_goodWords[i] = line.substr(start, end - start + 1);
+        }
+    }
 
-	void SpellChecker::operator()(std::string& text) {
-		for (size_t i = 0; i < 6; ++i) {
-			size_t pos = text.find(m_badWords[i]);
-			while (pos != std::string::npos) {
-				text.replace(pos, m_badWords[i].length(), m_goodWords[i]);
-				pos = text.find(m_badWords[i], pos + m_goodWords[i].length());
-				++m_replacements[i];
-			}
-		}
-	}
+    void SpellChecker::operator()(std::string& text) {
+        for (size_t i = 0; i < 6; ++i) {
+            size_t pos = text.find(m_badWords[i]);
+            while (pos != std::string::npos) {
+                text.replace(pos, m_badWords[i].length(), m_goodWords[i]);
+                pos = text.find(m_badWords[i], pos + m_goodWords[i].length());
+                ++m_replacements[i];
+            }
+        }
+    }
 
-	void SpellChecker::showStatistics(std::ostream& out) const {
-		for (size_t i = 0; i < 6; ++i) {
+    void SpellChecker::showStatistics(std::ostream& out) const noexcept {
+        for (size_t i = 0; i < 6; ++i) {
             out << std::right << std::setw(15) << m_badWords[i] << ": " << m_replacements[i] << " replacements\n";
         }
-	}
+    }
 }
--- W5P2Solution/W5P2/SpellChecker.h
+++ W5P2Solution/W5P2/SpellChecker.h
@@ -1,19 +1,20 @@
-#ifndef SENECA_SPELLCHECKER_H
-#define SENECA_SPELLCHECKER_H
+#ifndef SPELLCHECKER_H
+#define SPELLCHECKER_H
 
 #include <string>
+#include <iostream>
 
 namespace seneca {
-	class SpellChecker {
-		std::string m_badWords[6];
-		std::string m_goodWords[6];
-		size_t m_replacements[6] = {0};
+    class SpellChecker {
+        std::string m_badWords[6];
+        std::string m_goodWords[6];
+        size_t m_replacements[6] = { 0 };
 
-	public:
-		SpellChecker(const char* filename);
-		void operator()(std::string& text);
-		void showStatistics(std::ostream& out) const;
-	};
+    public:
+        SpellChecker(const char* filename);
+        void operator()(std::string& text);
+        void showStatistics(std::ostream& out) const noexcept;
+    };
 }
 
-#endif // !SENECA_SPELLCHECKER_H
+#endif // SPELLCHECKER_H
