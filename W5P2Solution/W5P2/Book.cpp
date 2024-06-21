df25bbbb15d086d1c81045860626235221f6f999
bruno27866 <bamaral2.git2023@gmail.com>
2024-06-21 19:07:46 -0400

    finishing the first part from third term

--- W5P2Solution/W5P2/Book.cpp
+++ W5P2Solution/W5P2/Book.cpp
@@ -19,12 +19,12 @@ piece of work is entirely of my own creation.
 #include "Book.h"
 
 namespace seneca {
-	Book::Book() : m_author(""), m_title(""), m_country(""), m_year(0), m_price(0), m_description("") {}
+	Book::Book() : m_author(""), m_title(""), m_country(""), m_year(0), m_price(0), m_description("") {} // Inicialização de lista de membros no construtor padrão
 
 	Book::Book(std::string author, std::string title, std::string country, int year, double price, std::string description) :
-		m_author(author), m_title(title), m_country(country), m_year(year), m_price(price), m_description(description) {}
+		m_author(author), m_title(title), m_country(country), m_year(year), m_price(price), m_description(description) {} // Inicialização de lista de membros no construtor parametrizado
 
-	Book::Book(const Book& book) {
+	Book::Book(const Book& book) { // Construtor de cópia
 		m_author = book.m_author;
 		m_title = book.m_title;
 		m_country = book.m_country;
@@ -33,8 +33,8 @@ namespace seneca {
 		m_description = book.m_description;
 	}
 
-	Book& Book::operator=(const Book& book) {
-		if(this != &book) {
+	Book& Book::operator=(const Book& book) { // Operador de atribuição de cópia
+		if (this != &book) {
 			m_author = book.m_author;
 			m_title = book.m_title;
 			m_country = book.m_country;
@@ -45,61 +45,60 @@ namespace seneca {
 		return *this;
 	}
 
-	Book::~Book() {}
+	Book::~Book() {} // Destrutor
 
-	Book::Book(const std::string& strBook) {
+	Book::Book(const std::string& strBook) { // Construtor que aceita uma string formatada
 		size_t start = 0;
 		size_t end = strBook.find(',');
 
-		// Extracting the author
+		// Extraindo o autor
 		std::string author = strBook.substr(start, end - start);
-		author.erase(0, author.find_first_not_of(' ')); // Trim leading spaces
-		author.erase(author.find_last_not_of(' ') + 1); // Trim trailing spaces
+		author.erase(0, author.find_first_not_of(' ')); // Remove espaços iniciais
+		author.erase(author.find_last_not_of(' ') + 1); // Remove espaços finais
 		m_author = author;
 
-
-		// Extracting the title
+		// Extraindo o título
 		start = end + 1;
 		end = strBook.find(',', start);
 		std::string title = strBook.substr(start, end - start);
-		title.erase(0, title.find_first_not_of(' ')); // Trim leading spaces
-		title.erase(title.find_last_not_of(' ') + 1); // Trim trailing spaces
+		title.erase(0, title.find_first_not_of(' ')); // Remove espaços iniciais
+		title.erase(title.find_last_not_of(' ') + 1); // Remove espaços finais
 		m_title = title;
 
-		// Extracting the country
+		// Extraindo o país
 		start = end + 1;
 		end = strBook.find(',', start);
 		std::string country = strBook.substr(start, end - start);
-		country.erase(0, country.find_first_not_of(' ')); // Trim leading spaces
-		country.erase(country.find_last_not_of(' ') + 1); // Trim trailing spaces
+		country.erase(0, country.find_first_not_of(' ')); // Remove espaços iniciais
+		country.erase(country.find_last_not_of(' ') + 1); // Remove espaços finais
 		m_country = country;
 
-		// Extracting the price
+		// Extraindo o preço
 		start = end + 1;
 		end = strBook.find(',', start);
 		std::string price = strBook.substr(start, end - start);
-		price.erase(0, price.find_first_not_of(' ')); // Trim leading spaces
-		price.erase(price.find_last_not_of(' ') + 1); // Trim trailing spaces
+		price.erase(0, price.find_first_not_of(' ')); // Remove espaços iniciais
+		price.erase(price.find_last_not_of(' ') + 1); // Remove espaços finais
 		m_price = std::stod(price);
 
-		// Extracting the year
+		// Extraindo o ano
 		start = end + 1;
 		end = strBook.find(',', start);
 		std::string year = strBook.substr(start, end - start);
-		year.erase(0, year.find_first_not_of(' ')); // Trim leading spaces
-		year.erase(year.find_last_not_of(' ') + 1); // Trim trailing spaces
+		year.erase(0, year.find_first_not_of(' ')); // Remove espaços iniciais
+		year.erase(year.find_last_not_of(' ') + 1); // Remove espaços finais
 		m_year = std::stoi(year);
 
-		// Extracting the description
+		// Extraindo a descrição
 		start = end + 1;
 		end = strBook.find('\n', start);
 		std::string description = strBook.substr(start, end - start);
-		description.erase(0, description.find_first_not_of(' ')); // Trim leading spaces
-		description.erase(description.find_last_not_of(' ') + 1); // Trim trailing spaces
+		description.erase(0, description.find_first_not_of(' ')); // Remove espaços iniciais
+		description.erase(description.find_last_not_of(' ') + 1); // Remove espaços finais
 		m_description = description;
 	}
 
-	std::ostream& operator<<(std::ostream& os, const Book& book) {
+	std::ostream& operator<<(std::ostream& os, const Book& book) { // Sobrecarregando o operador de inserção
 		os << std::setw(20) << std::setfill(' ') << std::right << book.m_author;
 		os << " | ";
 		os << std::setw(22) << std::setfill(' ') << std::right << book.m_title;
--- W5P2Solution/W5P2/Book.h
+++ W5P2Solution/W5P2/Book.h
@@ -11,41 +11,44 @@ shared with any other student or 3rd party content provider. This submitted
 piece of work is entirely of my own creation.
 /////////////////////////////////////////////////////////////////////////*/
 
-#ifndef SENECA_BOOK_H
+#ifndef SENECA_BOOK_H // Diretiva de pré-processador para evitar inclusões múltiplas
 #define SENECA_BOOK_H
 
 #include <iostream>
 #include <iomanip>
 #include <string>
 
-namespace seneca {
-	class Book {
+namespace seneca { // Uso de namespace para evitar conflitos de nome
+	class Book { // Definição de uma classe chamada Book
 	private:
-		std::string m_author;
+		std::string m_author; // Membros de dados privados
 		std::string m_title;
 		std::string m_country;
 		size_t m_year;
 		double m_price;
 		std::string m_description;
+
 	public:
-		Book();
-		Book(std::string author, std::string title, std::string country, int year, double price, std::string description);
-		Book(const Book& book);
-		Book& operator=(const Book& book);
-		~Book();
+		Book(); // Construtor padrão
+		Book(std::string author, std::string title, std::string country, int year, double price, std::string description); // Construtor parametrizado
+		Book(const Book& book); // Construtor de cópia
+		Book& operator=(const Book& book); // Operador de atribuição de cópia
+		~Book(); // Destrutor
 
-		std::string getAuthor() const { return m_author; };
-		std::string getDescription() const { return m_description; };
+		// Métodos de acesso (getters)
+		std::string getAuthor() const { return m_author; }
+		std::string getDescription() const { return m_description; }
 
-		const std::string& title() const { return m_title; };
-		const std::string& country() const { return m_country; };
-		const size_t& year() const { return m_year; };
-		double& price()	{ return m_price; };
+		const std::string& title() const { return m_title; }
+		const std::string& country() const { return m_country; }
+		const size_t& year() const { return m_year; }
+		double& price() { return m_price; }
 
-		Book(const std::string& strBook);
+		Book(const std::string& strBook); // Construtor que aceita uma string formatada
 
-		friend std::ostream& operator<<(std::ostream& os, const Book& book);
+		friend std::ostream& operator<<(std::ostream& os, const Book& book); // Declaração de função amiga para sobrecarregar o operador de inserção
 
+		// Template de método para corrigir a ortografia (exemplo de template de função)
 		template<typename T>
 		void fixSpelling(T& spellChecker) {
 			spellChecker(m_description);
@@ -54,3 +57,4 @@ namespace seneca {
 }
 
 #endif // !SENECA_BOOK_H
+
