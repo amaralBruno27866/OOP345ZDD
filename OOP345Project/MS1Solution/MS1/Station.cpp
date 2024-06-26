b4e1bfc06e399d0966f235f85e7ed2aa5fa3a563
bruno27866 <bamaral2.git2023@gmail.com>
2024-06-26 11:29:46 -0400

    implementation: all functions from Station module was implemented

--- OOP345Project/MS1Solution/MS1/Station.cpp
+++ OOP345Project/MS1Solution/MS1/Station.cpp
@@ -1,27 +1,61 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include <string>
+#include <iomanip>
 #include "Station.h"
 
 namespace seneca {
+	size_t Station::m_widthField = 0;
+	int Station::id_generator = 0;
+
 	Station::Station(const std::string& record) {
+		Utilities u;
+		size_t nexr_pos = 0;
+		bool more = true;
+
+		m_itemName = u.extractToken(record, nexr_pos, more);
+		m_serialNumber = std::stoi(u.extractToken(record, nexr_pos, more));
+		m_quantityInStock = std::stoi(u.extractToken(record, nexr_pos, more));
+
+		if (more) {
+			std::string desc = u.extractToken(record, nexr_pos, more);
+			m_stationDesc = desc;
+			m_widthField = std::max(m_widthField, u.getFieldWidth());
+		}
+
+		m_id = ++id_generator;
 	}
 
 	const std::string& Station::getItemName() const {
-		// TODO: insert return statement here
+		return m_itemName;
 	}
 
 	size_t Station::getNextSerialNumber() {
-		return size_t();
+		return m_serialNumber++;
 	}
 
 	size_t Station::getQuantity() const {
-		return size_t();
+		return m_quantityInStock;
 	}
 
 	void Station::updateQuantity() {
+		if (m_quantityInStock > 0) {
+			--m_quantityInStock;
+		}
 	}
 
 	void Station::display(std::ostream& os, bool full) const {
+		os << std::setw(3) << std::setfill('0') << m_id;
+		os << " | ";
+		os << std::setw(m_widthField) << std::setfill(' ') << m_itemName;
+		os << " | ";
+		os << std::setw(6) << std::setfill('0') << m_serialNumber;
+		os << " | ";
+		if (full) {
+			os << std::setw(4) << std::setfill(' ') << m_quantityInStock;
+			os << " | ";
+			os << m_stationDesc;
+		}
+		os << std::endl;
 	}
 }
