8b5e0cc14d41d1d31b5d35034812a718fd5efa1a
bruno27866 <bamaral2.git2023@gmail.com>
2024-06-26 11:06:54 -0400

    implemented: Implement Utilities Module for String Parsing

--- OOP345Project/MS1Solution/MS1/Utilities.h
+++ OOP345Project/MS1Solution/MS1/Utilities.h
@@ -5,14 +5,24 @@
 #include <string>
 
 namespace seneca {
+	// The Utilities class provides helper functions for parsing strings.
 	class Utilities {
-		size_t m_widthField = 1;
-		static char m_delimiter;
+		size_t m_widthField = 1; // Current maximum field width
+		static char m_delimiter; // Delimiter used for token extraction
 	public:
+		// Sets the field width of the current object
 		void setFieldWidth(size_t newWidth);
+
+		// Returns the field width of the current object
 		size_t getFieldWidth() const;
+
+		// Extracts tokens from the first parameter.
 		std::string extractToken(const std::string& str, size_t& next_pos, bool& more);
+
+		// Sets the delimiter for this class to the character received
 		static void setDelimiter(char newDelimiter);
+
+		// Returns the delimiter character of the current object
 		static char getDelimiter();
 	};
 }
