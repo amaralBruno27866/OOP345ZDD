1c2f3dc6de83942c95b8f66fb25bb0b7d90f0d75
bruno27866 <bamaral2.git2023@gmail.com>
2024-07-20 15:52:32 -0400

    create: folder for workshop 8 part 2

---
+++ W8P2Solution/W8P2/Database.h
@@ -0,0 +1,33 @@
+#ifndef SENECA_DATABASE_H
+#define SENECA_DATABASE_H
+
+#include <cstring>
+#include <string>
+#include <memory>
+
+namespace seneca {
+	enum class Err_Status
+	{
+		Err_Success,
+		Err_NotFound,
+		Err_OutOfMemory,
+	};
+
+	class Database {
+	private:
+		static std::shared_ptr<Database> instance;
+		std::string keys[20];
+		std::string values[20];
+		std::string filename;
+		int entryCount = 0;
+
+		Database(const std::string& filename);
+	public:
+		static std::shared_ptr<Database> getInstance(const std::string& filename);
+		Err_Status GetValue(const std::string& key, std::string& value);
+		Err_Status SetValue(const std::string& key, const std::string& value);
+		~Database();
+	};
+}
+
+#endif // !SENECA_DATABASE_H
