6e1e160d96f69bc50129d4950f6d21ec8366d5fa
bruno27866 <bamaral2.git2023@gmail.com>
2024-07-20 20:16:53 -0400

    add: reflect.txt file

--- W8P2Solution/W8P2/Database.h
+++ W8P2Solution/W8P2/Database.h
@@ -1,3 +1,16 @@
+/*/////////////////////////////////////////////////////////////////////////
+				  Workshop - #8 P2
+Full Name  : Bruno Amaral
+Student ID#: 143766228
+Email      : bamaral2@myseneca.ca
+Section    : ZDD
+
+Authenticity Declaration:
+I declare this submission is the result of my own work and has not been
+shared with any other student or 3rd party content provider. This submitted
+piece of work is entirely of my own creation.
+/////////////////////////////////////////////////////////////////////////*/
+
 #ifndef SENECA_DATABASE_H
 #define SENECA_DATABASE_H
 
@@ -27,24 +40,18 @@ namespace seneca {
 		std::string filename;
 		int entryCount = 0;
 
-		// Private constructor
 		Database(const std::string& filename) : filename(filename), entryCount(0) {
 			std::cout << "[" << this << "] Database(const std::string&)\n";
+			std::fill_n(values, 20, T{});
 			std::ifstream file(filename);
 			std::string line;
 			while (std::getline(file, line)) {
 				std::string key, value;
-
-				// Find the position of the first space to separate the key and value
 				size_t spacePos = line.find(' ');
 				if (spacePos != std::string::npos) {
 					key = line.substr(0, spacePos);
 					value = line.substr(spacePos + 1);
-
-					// Replace underscores with spaces in the key
 					std::replace(key.begin(), key.end(), '_', ' ');
-
-					// Store the key and value in the arrays if there's space
 					if (entryCount < 20) {
 						keys[entryCount] = key;
 						std::istringstream iss(value);
@@ -57,15 +64,12 @@ namespace seneca {
 			}
 		}
 
-		// Encrypt/Decrypt function
 		void encryptDecrypt(T& val) {}
 
 	public:
-		// Deleted copy constructor and assignment operator to enforce singleton pattern
 		Database(const Database&) = delete;
 		Database& operator=(const Database&) = delete;
 
-		// Singleton getInstance method
 		static std::shared_ptr<Database<T>> getInstance(const std::string& filename) {
 			if (!instance) {
 				instance = std::shared_ptr<Database<T>>(new Database<T>(filename));
@@ -73,12 +77,11 @@ namespace seneca {
 			return instance;
 		}
 
-		// Public interface for getting and setting values
 		Err_Status GetValue(const std::string& key, T& value) {
 			for (int i = 0; i < entryCount; ++i) {
 				if (keys[i] == key) {
 					value = values[i];
-					encryptDecrypt(value); // Decrypt the value
+					encryptDecrypt(value);
 					return Err_Status::Err_Success;
 				}
 			}
@@ -95,7 +98,6 @@ namespace seneca {
 			return Err_Status::Err_Success;
 		}
 
-		// Destructor
 		~Database() {
 			std::cout << "[" << this << "] ~Database()\n";
 			std::ofstream backupFile(filename + ".bkp.txt");
