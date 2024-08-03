c9983e43dd6ee59e98565b398a1e902dd8200d13
bruno27866 <bamaral2.git2023@gmail.com>
2024-08-03 11:37:28 -0400

    fix: display was wrong

--- OOP345Project/MS3Solution/MS3/CustomerOrder.cpp
+++ OOP345Project/MS3Solution/MS3/CustomerOrder.cpp
@@ -136,7 +136,7 @@ namespace seneca {
 		os << m_name << " - " << m_product << std::endl;
 
 		for (size_t i = 0; i < m_cntItem; i++) {
-			os << "[" << std::setw(6) << std::setfill('0') << m_lstItem[i]->m_serialNumber << "] ";
+			os << "[" << std::setw(6) << std::right << std::setfill('0') << m_lstItem[i]->m_serialNumber << "] ";
 			os << std::left << std::setw(m_widthField) << std::setfill(' ') << m_lstItem[i]->m_itemName;
 			os << " - ";
 			if(m_lstItem[i]->m_isFilled) {
